6857cac2dc77fb1d70c142c6caac2d2092245319
irassl <smith.ya-s@yandex.ru>
2022-03-15 15:31:49 +0300

    Updates

---
+++ precache-manifest.2412323b79393eaa42a818137e0d40bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93edefa26f54b3cc3ca2841395e492f9",
+    "url": "/todolostjsx/index.html"
+  },
+  {
+    "revision": "f5bfdbe3852b58484f6d",
+    "url": "/todolostjsx/static/css/main.fe4e5ca4.chunk.css"
+  },
+  {
+    "revision": "3649be2dc8b8e3193387",
+    "url": "/todolostjsx/static/js/2.d99582fe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolostjsx/static/js/2.d99582fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5bfdbe3852b58484f6d",
+    "url": "/todolostjsx/static/js/main.6a4cfbeb.chunk.js"
+  },
+  {
+    "revision": "4c5e6e34cc44adce1471",
+    "url": "/todolostjsx/static/js/runtime-main.64128e26.js"
+  }
+]);
